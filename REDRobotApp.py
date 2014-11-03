2fbac71c862cddb5180c693931abe025f25018e6
Jakob <jhoell@umich.edu>
2014-11-03 16:12:37 -0500

    stuff:

--- REDRobotApp.py
+++ REDRobotApp.py
@@ -17,7 +17,7 @@ from joy import progress
 from pdb import set_trace as DEBUG
 from sensorPlan import SensorPlan
 
-#from REDRobotControlPlan_improved import REDRobotControlPlan
+from REDRobotControlPlan_Improved import REDRobotControlPlan
 
 class REDRobotApp( JoyApp):
 
@@ -33,30 +33,29 @@ class REDRobotApp( JoyApp):
 
     def onStart(self):
         print "Initialzing the robot"
-        #self.sensor = SensorPlan(self)
-        #self.sensor.start()
-       #self.controller = REDRobotControlPlan(self,self.bot)
-        #self.controller.start()
-        #self.timeForUpdate = self.onceEvery(1/20.0)
+        self.sensor = SensorPlan(self)
+        self.sensor.start()
+        self.timeForUpdate = self.onceEvery(1/20.0)
         #self.bot = REDRobotDriver(right_wheel, #right wheel
         #                            left_wheel,  #left wheel
         #                            laserServo, #laser -- placeholder
         #                            tagServo,  #tag -- placeholder
         #                            0.6) #torque
         self.bot = REDRobotDriver(app,self.rw,self.lw,self.lzr,self.tag,0.3)
+       #self.controller = REDRobotControlPlan(self,self.bot)
         self.bot.start()
 
     def onEvent(self, evt):
 
-        # if self.timeForUpdate():
-        #     #get the latest information from sensors and update the controller
-        #     #with those values
-        #     ts,f,b = self.sensor.lastSensor
-        #     ts2, w = self.sensor.lastWaypoint
-        #     if ts:
-        #         self.controller.update_waypoint(w)
-        #     if ts2:
-        #         self.controller.update_sensor_values(f,b)
+        #if self.timeForUpdate():
+            #get the latest information from sensors and update the controller
+            #with those values
+            #ts,f,b = self.sensor.lastSensor
+           #ts2, w = self.sensor.lastWaypoint
+            #if ts:
+            #    self.controller.update_waypoint(w)
+            #if ts2:
+            #    self.controller.update_sensor_values(f,b)
 
 
         if evt.type == KEYDOWN:
@@ -67,23 +66,28 @@ class REDRobotApp( JoyApp):
                 self.bot.robotMove(5,-1)
                 return progress("(say) Move back")
             elif evt.key == K_LEFT:
-                self.bot.robotTurn(-pi/2)
+                #self.bot.robotTurn(-pi/2)
+                self.bot.unitRobotRotate(1)
+                self.bot.unitLaserRotate(1)
                 return progress("(say) Turn left")
             elif evt.key == K_RIGHT:
-                self.bot.robotTurn(pi/2)
-                #self.bot.unitRobotRotate(-1)
-                #self.bot.unitLaserRotate(-1)
+                #self.bot.robotTurn(pi/2)
+                self.bot.unitRobotRotate(-1)
+                self.bot.unitLaserRotate(-1)
                 return progress("(say) Turn right")
             elif evt.key == K_q:
-                #self.bot.unitTagRotate(1)
-                self.bot.tagRotate(pi/2.0)
+                self.bot.unitTagRotate(1)
+                #self.bot.tagRotate(pi/2.0)
                 return progress("(say) Turning tag")
             elif evt.key == K_w:
-                self.bot.laserRotate(pi/2.0)
+                self.bot.laserRotate(-pi/8.0)
                 return progress("(say) Turning laser")
             elif evt.key == K_a:
+                self.controller.start()
                 self.controller.go_autonomous()
                 return progress("(say) Going Autonomous!")
+            elif evt.key == K_m:
+                self.controller.autonomous = False
             # Use superclass to show any other events
             return JoyApp.onEvent(self,evt)
 
